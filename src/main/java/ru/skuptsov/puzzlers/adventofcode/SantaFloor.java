6716cc1a100b567ae5d032c67b651071b1f50813
kuptservol <Matreshka99>
2016-03-16 22:49:09 +0300

    elves

--- src/main/java/ru/skuptsov/puzzlers/adventofcode/SantaFloor.java
+++ src/main/java/ru/skuptsov/puzzlers/adventofcode/SantaFloor.java
@@ -21,15 +21,20 @@ public class SantaFloor {
         Reader reader = new InputStreamReader(SantaFloor.class.getClass().getResourceAsStream("/santaInput.txt"));
 
         int i = 0;
+        int position = 0;
         int parentheses = reader.read();
         for (; parentheses != -1; parentheses = reader.read()) {
+            position++;
             if ((char) parentheses == '(')
                 i++;
             if ((char) parentheses == ')')
                 i--;
+
+            if(i==-1)
+                break;
         }
 
-        System.out.println(i);
+        System.out.println(position);
     }
 }
 
