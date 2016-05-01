613982ff53d5ff733dcb2a20df8536033ecaca1f
kuptservol <Matreshka99>
2016-05-01 23:03:20 +0300

    m

---
+++ src/main/java/ru/skuptsov/algorythms/queue/MinMaxPriorityQueueTest.java
@@ -0,0 +1,23 @@
+package ru.skuptsov.algorythms.queue;
+
+import java.util.Random;
+
+/**
+ * @author Sergey Kuptsov
+ * @since 01/05/2016
+ */
+public class MinMaxPriorityQueueTest {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] massive = new int[1000000000];
+        for (int i = 0; i < massive.length; i++) {
+            massive[i] = random.nextInt(1000000000);
+        }
+
+        MinMaxPriorityQueueTest
+
+    }
+}
---
+++ src/main/java/ru/skuptsov/patterns/behavioral/strategy/Strategy.java
@@ -0,0 +1,8 @@
+package ru.skuptsov.patterns.behavioral.strategy;
+
+/**
+ * @author Sergey Kuptsov
+ * @since 01/05/2016
+ */
+public class Strategy {
+}
