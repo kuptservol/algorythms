24a90558f5d2646bf8010142f3ce4dba43abdb06
kuptservol <kuptservol@gmail.com>
2016-05-12 23:11:26 +0300

    chain_of_responsibility

---
+++ src/main/java/ru/skuptsov/Test.java
@@ -0,0 +1,16 @@
+package ru.skuptsov;
+
+import java.util.Random;
+import java.util.stream.LongStream;
+
+/**
+ * Created by Сергей on 12.05.2016.
+ */
+public class Test
+{
+
+    public static void main(String[] args) {
+
+        System.out.println(new Random().ints());
+    }
+}
