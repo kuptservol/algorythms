be808e7eea7f99a6aec6c225ccabe4b4a5e2c5cf
kuptservol <Matreshka99>
2016-05-14 23:19:54 +0300

    recursive descent parser

---
+++ src/main/java/ru/skuptsov/puzzlers/job/interview/redmart/Cell.java
@@ -0,0 +1,54 @@
+package ru.skuptsov.puzzlers.job.interview.redmart;
+
+/**
+ * @author Sergey Kuptsov
+ * @since 14/05/2016
+ */
+
+/**
+ * Sheet cell
+ */
+public final class Cell {
+
+    /**
+     * Cell expression Value
+     */
+    private double value;
+    /**
+     * String cell text
+     */
+    private String text;
+    /**
+     * Identifies if cell already have been counted
+     */
+    private boolean hasCalculatedValue;
+
+
+    public Cell(String text) {
+        this.text = text;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isHasCalculatedValue() {
+        return hasCalculatedValue;
+    }
+
+    public void setHasCalculatedValue(boolean hasCalculatedValue) {
+        this.hasCalculatedValue = hasCalculatedValue;
+    }
+}
---
+++ src/main/java/ru/skuptsov/puzzlers/job/interview/redmart/Helper.java
@@ -0,0 +1,50 @@
+package ru.skuptsov.puzzlers.job.interview.redmart;
+
+/**
+ * @author Sergey Kuptsov
+ * @since 14/05/2016
+ */
+public class Helper {
+
+
+    /**
+     * Check if token is cell link
+     *
+     * @param token
+     * @return
+     */
+    public static boolean isCellLink(String token) {
+
+        try {
+
+            char ch0 = token.charAt(0);
+            int temp = (int) ch0;
+            if (!(temp <= 90 & temp >= 65))
+                return false;
+            if (!Character.isAlphabetic(ch0))
+                return false;
+            String ch1_n = token.substring(1, token.length());
+            if (!isNumeric(ch1_n))
+                return false;
+
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Check if string value is numeric
+     *
+     * @param str number string value
+     * @return true if numeric
+     */
+    public static boolean isNumeric(String str) {
+        try {
+            double d = Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
