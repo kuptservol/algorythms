4723c9526b20381351ac0b82c4158176d61dc2f8
kuptservol <Matreshka99>
2016-05-21 15:53:08 +0300

    mediator

---
+++ src/main/java/ru/skuptsov/patterns/behavioral/mediator/ChatRoomMediator.java
@@ -0,0 +1,42 @@
+package ru.skuptsov.patterns.behavioral.mediator;
+
+import java.util.Scanner;
+
+/**
+ * @author Sergey Kuptsov
+ * @since 21/05/2016
+ */
+public class ChatRoomMediator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("print username");
+
+        User user = new User(scanner.next());
+        System.out.println("now you can start chating");
+
+        ChatRoom chatRoom = new ChatRoom();
+
+        while (scanner.hasNextLine()) {
+            chatRoom.sendMessage(user, scanner.nextLine());
+        }
+    }
+
+    private static class User {
+        private final String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private static class ChatRoom {
+        public void sendMessage(User user, String message) {
+            System.out.println(user.getName() + ":" + message);
+        }
+    }
+}
