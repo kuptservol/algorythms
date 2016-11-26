8d54aff8fe364eeee67163baf345a45daeb58d2b
kuptservol <skuptsov@zmeke.com>
2016-11-26 21:35:44 +0300

    * course schedule

--- src/main/java/ru/skuptsov/puzzlers/job/interview/programcreek/graph/CourseSchedule.java
+++ src/main/java/ru/skuptsov/puzzlers/job/interview/programcreek/graph/CourseSchedule.java
@@ -3,7 +3,8 @@ package ru.skuptsov.puzzlers.job.interview.programcreek.graph;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static com.google.common.collect.ImmutableList.of;
 import static org.testng.Assert.assertEquals;
@@ -33,25 +34,62 @@ public class CourseSchedule {
     @DataProvider
     public Object[][] testData() {
         return new Object[][]{
+                {of(of(0, 1, 2), of(1, 3, 4), of(2, 5, 6)), true},
+                {of(of(0, 1, 2), of(1, 3, 4), of(2, 5, 6), of(3, 6)), false},
                 {of(of(2), of(1, 0)), true},
-                {of(of(0, 1), of(1, 0)), false}
+                {of(of(0, 1), of(1, 0)), false},
+                {of(of(0, 5, 4, 3, 2, 1), of(5, 0)), false},
         };
     }
 
     @Test(dataProvider = "testData")
-    public void test(List<List> courses, boolean isPossible) {
+    public void test(List<List<Integer>> courses, boolean isPossible) {
         assertEquals(hasCycle(buildGraph(courses)), isPossible);
     }
 
-    private Graph buildGraph(List<List> courses) {
-        return null;
-    }
-
     private boolean hasCycle(Graph graph) {
-        return false;
+        Queue<Integer> traversalQueue = new LinkedBlockingQueue<>();
+
+        Integer startV = graph.adjList.keySet().toArray(new Integer[0])[0];
+        Set<Integer> visitedNodes = new HashSet<>();
+
+        traversalQueue.add(startV);
+
+        while (!traversalQueue.isEmpty()) {
+            Integer nextV = traversalQueue.poll();
+            if (visitedNodes.contains(nextV)) {
+                return false;
+            } else {
+                visitedNodes.add(nextV);
+            }
+
+            System.out.println(nextV);
+            for (Integer linkedV : graph.adjList.get(nextV)) {
+                traversalQueue.add(linkedV);
+            }
+        }
+
+        return true;
     }
 
-    private final static class Graph {
+    private Graph buildGraph(List<List<Integer>> courses) {
+        Graph graph = new Graph();
 
+        for (List<Integer> targetCourses : courses) {
+            Integer v = targetCourses.get(0);
+            graph.adjList.putIfAbsent(v, new HashSet<>());
+
+            for (int i = 1; i < targetCourses.size(); i++) {
+                Integer linkedV = targetCourses.get((i));
+                graph.adjList.putIfAbsent(linkedV, new HashSet<>());
+                graph.adjList.get(v).add(linkedV);
+            }
+        }
+
+        return graph;
+    }
+
+    private static class Graph {
+        Map<Integer, Set<Integer>> adjList = new HashMap<>();
     }
 }
