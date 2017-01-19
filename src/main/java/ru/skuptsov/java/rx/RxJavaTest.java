0239fa0b523167a4fc492ba0afd6cb3b250545a4
kuptservol <skuptsov@zmeke.com>
2017-01-19 23:37:46 +0300

    * More RxJava, more! * HammingWeight

--- src/main/java/ru/skuptsov/java/rx/RxJavaTest.java
+++ src/main/java/ru/skuptsov/java/rx/RxJavaTest.java
@@ -1,8 +1,14 @@
 package ru.skuptsov.java.rx;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.testng.annotations.Test;
 import rx.Observable;
 import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Sergey Kuptsov
@@ -76,5 +82,78 @@ public class RxJavaTest {
             return "2";
         else
             throw new RuntimeException();
+    }
+
+    @Test
+    public void withBackPressure() {
+        Observable.just("1", "2")
+                .subscribe(new Subscriber<String>() {
+
+                    @Override
+                    public void onStart() {
+                        request(1);
+                    }
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.println(s);
+                        request(1);
+                    }
+                });
+    }
+
+    @Test
+    public void bufferWithParallelization() throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(
+                5,
+                new ThreadFactoryBuilder().setDaemon(true).setNameFormat("PushClientQueueProcessor-%d").build());
+
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i < 100; i++) {
+                    subscriber.onNext(i);
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                subscriber.onCompleted();
+            }
+        })
+                .buffer(5, TimeUnit.SECONDS, 4)
+                .flatMap(l -> Observable.just(l)
+                        .subscribeOn(Schedulers.from(executorService))
+                        .map(s -> {
+                                    System.out.println(Thread.currentThread().getName());
+                                    System.out.println(Thread.currentThread().getId());
+                                    try {
+                                        Thread.sleep(5000);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    System.out.println(s);
+
+                                    return null;
+                                }
+                        )
+                ).subscribe();
+
+        Thread.sleep(50000);
     }
 }
