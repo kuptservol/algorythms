4715b3d0fc46e3ad929fc8c36da361e6b256494a
kuptservol <skuptsov@zmeke.com>
2017-01-25 22:38:20 +0300

    * stamped lock test

--- src/main/java/ru/skuptsov/java/benchmark/R19W1LockSpeedTest.java
+++ src/main/java/ru/skuptsov/java/benchmark/R19W1LockSpeedTest.java
@@ -12,6 +12,13 @@ import static org.openjdk.jmh.annotations.Mode.AverageTime;
 @State(value = Scope.Benchmark)
 @Warmup(iterations = 1)
 @Measurement(iterations = 1)
+/**
+ * Benchmark                                                 Mode  Cnt        Score         Error  Units
+ R19W1LockSpeedTest.rWReentrantRWLockSpeedTest             avgt   10  2415087,031 ± 2669636,447  us/op
+ R19W1LockSpeedTest.stampedOptimisticLockRWLockSpeedTest   avgt   10   471373,962 ±  744752,311  us/op
+ R19W1LockSpeedTest.stampedPessimisticLockRWLockSpeedTest  avgt   10    58546,341 ±   19897,359  us/op
+ R19W1LockSpeedTest.syncRWLockSpeedTest                    avgt   10    99922,651 ±    8859,416  us/op
+ */
 public class R19W1LockSpeedTest extends BaseRWLockSpeedTest {
 
     public R19W1LockSpeedTest() {
