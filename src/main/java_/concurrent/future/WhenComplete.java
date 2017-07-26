25ccb8945796300273c606d6dd55f9f25ab22ea8
Sergey Kuptsov <kuptservol@yandex-team.ru>
2017-07-26 13:20:08 +0300

    * not obvious whenComplete

---
+++ src/main/java_/concurrent/future/WhenComplete.java
@@ -0,0 +1,157 @@
+package java_.concurrent.future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static com.google.common.collect.ImmutableList.of;
+
+/**
+ * @author Sergey Kuptsov <kuptservol@yandex-team.ru>
+ */
+public class WhenComplete {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        try {
+            CompletableFuture.allOf(of(
+                    CompletableFuture.runAsync(new RunnableOk()),
+                    CompletableFuture.runAsync(new RunnableOk()))
+                    .toArray(new CompletableFuture[]{}))
+                    .get(1, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("2");
+        try {
+            CompletableFuture.allOf(of(
+                    CompletableFuture.runAsync(new RunnableWithTimeout()),
+                    CompletableFuture.runAsync(new RunnableOk()))
+                    .toArray(new CompletableFuture[]{}))
+                    .get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException " + e.getMessage());
+        } catch (ExecutionException e) {
+            System.out.println("Execution Exception " + e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println("Timeout Exception " + e.getMessage());
+        }
+
+        System.out.println("3");
+        //exception lost
+
+        try {
+            CompletableFuture.allOf(of(
+                    CompletableFuture.runAsync(new RunnableWithException()),
+                    CompletableFuture.runAsync(new RunnableOk()))
+                    .toArray(new CompletableFuture[]{}))
+                    .get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException " + e.getMessage());
+        } catch (ExecutionException e) {
+            System.out.println("Execution Exception " + e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println("Timeout Exception " + e.getMessage());
+        }
+
+        System.out.println("4");
+
+        try {
+            CompletableFuture.allOf(of(
+                    CompletableFuture.runAsync(new RunnableWithException()),
+                    CompletableFuture.runAsync(new RunnableOk()))
+                    .toArray(new CompletableFuture[]{}))
+                    .whenComplete((res, ex) -> System.out.println("Comleted with exception : " + ex.getMessage()))
+                    .get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException " + e.getMessage());
+        } catch (ExecutionException e) {
+            System.out.println("Execution Exception " + e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println("Timeout Exception " + e.getMessage());
+        }
+
+        System.out.println("5");
+
+        // NPE here!!!!
+        try {
+            CompletableFuture.allOf(of(
+                    CompletableFuture.runAsync(new RunnableOk()),
+                    CompletableFuture.runAsync(new RunnableOk()))
+                    .toArray(new CompletableFuture[]{}))
+                    .whenComplete((res, ex) -> System.out.println("Completed with exception : " + ex.getMessage()))
+                    .get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException " + e.getMessage());
+        } catch (ExecutionException e) {
+            System.out.println("Execution Exception " + e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println("Timeout Exception " + e.getMessage());
+        }
+
+        System.out.println("5.1");
+
+        // NPE here!!!!
+        try {
+            CompletableFuture.allOf(of(
+                    CompletableFuture.runAsync(new RunnableOk()),
+                    CompletableFuture.runAsync(new RunnableOk()))
+                    .toArray(new CompletableFuture[]{}))
+                    .whenCompleteAsync((res, ex) -> System.out.println("Completed with exception : " + ex.getMessage()))
+                    .get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException " + e.getMessage());
+        } catch (ExecutionException e) {
+            System.out.println("Execution Exception " + e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println("Timeout Exception " + e.getMessage());
+        }
+
+        System.out.println("6");
+
+        try {
+            CompletableFuture.allOf(of(
+                    CompletableFuture.runAsync(new RunnableOk()),
+                    CompletableFuture.runAsync(new RunnableWithTimeout()))
+                    .toArray(new CompletableFuture[]{}))
+                    .whenComplete((res, ex) -> System.out.println("Comleted with exception : " + ex.getMessage()))
+                    .get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException " + e.getMessage());
+        } catch (ExecutionException e) {
+            System.out.println("Execution Exception " + e.getMessage());
+        } catch (TimeoutException e) {
+            System.out.println("Timeout Exception " + e.getMessage());
+        }
+    }
+
+    static class RunnableWithTimeout implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class RunnableWithException implements Runnable {
+
+        @Override
+        public void run() {
+            throw new RuntimeException("Exception");
+        }
+    }
+
+    static class RunnableOk implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Norm");
+        }
+    }
+}
