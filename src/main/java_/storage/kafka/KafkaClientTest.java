4abe4ef667ec2d8aa73f8d7687868f744022fd27
Sergey Kuptsov <kuptservol@yandex-team.ru>
2017-08-06 22:04:55 +0300

    * kafka client test

---
+++ src/main/java_/storage/kafka/KafkaClientTest.java
@@ -0,0 +1,30 @@
+package java_.storage.kafka;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class KafkaClientTest {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("java_test_partitioned"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s, partition=%d%n", record.offset(), record.key(), record.value(), record.partition());
+        }
+    }
+
+}
