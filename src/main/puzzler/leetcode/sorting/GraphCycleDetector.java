1654a656f1f89612344b58275b69ab37cc837372
Sergey Kuptsov <kuptservol@yandex-team.ru>
2018-04-24 21:05:02 +0300

    fix graph cycle detector

--- src/main/puzzler/leetcode/sorting/GraphCycleDetector.java
+++ src/main/puzzler/leetcode/sorting/GraphCycleDetector.java
@@ -1,9 +1,10 @@
 package puzzler.leetcode.sorting;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.Stack;
 
 import com.google.common.collect.ImmutableMap;
 import org.testng.annotations.DataProvider;
@@ -15,23 +16,39 @@ import static org.testng.Assert.assertEquals;
 public class GraphCycleDetector {
 
     public static boolean isContainsCycle(Map<Integer, Set<Integer>> graph) {
-        Set<Integer> callStack = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
+        Stack<Integer> currentStack = new Stack<>();
 
-        LinkedList<Integer> dfs = new LinkedList<>();
-
-        dfs.add(graph.keySet().iterator().next());
-
-        Integer node;
-        while ((node = dfs.pollLast()) != null) {
-            if (callStack.contains(node)) {
-                return true;
-            }
-            callStack.add(node);
-
-            if (graph.get(node) != null) {
-                dfs.addAll(graph.get(node));
+        for (Integer node : graph.keySet()) {
+            if (!visited.contains(node)) {
+                if (dfsCycleCheck(node, graph, visited, currentStack)) {
+                    return true;
+                }
             }
         }
+
+        return false;
+    }
+
+    public static boolean dfsCycleCheck(Integer currNode, Map<Integer, Set<Integer>> graph, Set<Integer> visited, Stack<Integer> currentStack) {
+        currentStack.push(currNode);
+        visited.add(currNode);
+
+        Optional<Integer> fromNode = currentStack.size() > 0 ? Optional.of(currentStack.peek()) : Optional.empty();
+        for (Integer nextNode : graph.getOrDefault(currNode, new HashSet<>())) {
+            if (fromNode.isPresent() && fromNode.get().equals(nextNode)) {
+                continue;
+            }
+            if (visited.contains(nextNode)) {
+                if (currentStack.contains(currNode)) {
+                    return true;
+                }
+            } else {
+                return dfsCycleCheck(nextNode, graph, visited, currentStack);
+            }
+        }
+
+        currentStack.pop();
 
         return false;
     }
@@ -40,7 +57,9 @@ public class GraphCycleDetector {
     public Object[][] testData() {
         return new Object[][]{
                 {ImmutableMap.of(0, of(5), 3, of(5), 5, of(4), 4, of(3)), true},
-                {ImmutableMap.of(0, of(5), 5, of(4), 4, of(3)), false}
+                {ImmutableMap.of(0, of(0), 3, of(5), 5, of(4), 4, of(3)), true},
+                {ImmutableMap.of(0, of(5), 5, of(4), 4, of(3)), false},
+                {ImmutableMap.of(0, of(0), 5, of(4), 4, of(3)), false}
         };
     }
 
