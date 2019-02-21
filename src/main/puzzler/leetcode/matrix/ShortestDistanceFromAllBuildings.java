1cfa9f3ef904af5b017b8adc921a339bf3e3b6f3
Sergey Kuptsov <kuptservol@yandex-team.ru>
2019-02-21 23:45:51 +0300

    * ShortestDistanceFromAllBuildings

---
+++ src/main/puzzler/leetcode/matrix/ShortestDistanceFromAllBuildings.java
@@ -0,0 +1,114 @@
+package puzzler.leetcode.matrix;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author Sergey Kuptsov
+ */
+public class ShortestDistanceFromAllBuildings {
+
+    int[][] grid1 = {
+            {1, 0, 2, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0}
+    };
+
+    int[][] grid2 = {{1, 2, 0}};
+
+    @DataProvider
+    public Object[][] testData() {
+        return new Object[][]{
+                {grid1, 7},
+                {grid2, -1}
+        };
+    }
+
+    @Test(dataProvider = "testData")
+    public void test(int[][] grid, int result) {
+        assertEquals(shortestDistance(grid), result);
+    }
+
+    /**
+     * for each building count sum distances to it from every point of matrix
+     * then find lowest value
+     * speed O(num_buildings*matrix_size) mem O(matrix_size)
+     */
+    public int shortestDistance(int[][] grid) {
+
+        int[][][] distances = new int[grid.length][grid[0].length][2];
+
+        int housesNum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == 2) {
+                    distances[i][j][0] = -1;
+                }
+
+                if (grid[i][j] == 1) {
+                    housesNum++;
+                    distances[i][j][0] = -1;
+
+                    // sum distances to (i,j) building with bfs
+                    Set<String> visited = new HashSet<>();
+                    LinkedList<int[]> bfs = new LinkedList<>();
+                    bfs.addLast(new int[]{i + 1, j, 0});
+                    bfs.addLast(new int[]{i - 1, j, 0});
+                    bfs.addLast(new int[]{i, j + 1, 0});
+                    bfs.addLast(new int[]{i, j - 1, 0});
+
+                    int[] next;
+                    while ((next = bfs.pollFirst()) != null) {
+                        int i_next = next[0];
+                        int j_next = next[1];
+                        if (i_next < 0 || j_next < 0 || i_next >= grid.length || j_next >= grid[0].length) {
+                            continue;
+                        }
+
+                        if (visited.contains(i_next + "_" + j_next)) {
+                            continue;
+                        } else {
+                            visited.add(i_next + "_" + j_next);
+                        }
+
+                        int prev = next[2] + 1;
+                        if (grid[i_next][j_next] == 0) {
+                            distances[i_next][j_next][0] += prev;
+                            //count num of buildings that reached point
+                            distances[i_next][j_next][1]++;
+
+                            bfs.addLast(new int[]{i_next + 1, j_next, prev});
+                            bfs.addLast(new int[]{i_next - 1, j_next, prev});
+                            bfs.addLast(new int[]{i_next, j_next + 1, prev});
+                            bfs.addLast(new int[]{i_next, j_next - 1, prev});
+                        }
+                    }
+                }
+            }
+        }
+
+        // find min distance pos if not -1
+        int min = -1;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                // this point was reached from every bulding
+                if (distances[i][j][1] == housesNum) {
+                    if (min == -1) {
+                        min = distances[i][j][0];
+                    } else {
+                        min = Math.min(distances[i][j][0], min);
+                    }
+                }
+            }
+        }
+
+        return min;
+    }
+}
