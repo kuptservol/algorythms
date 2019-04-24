cfbf0a24e9fd7d21a502451de4bf9231e17d3642
Sergey Kuptsov <kuptservol@yandex-team.ru>
2019-04-24 23:43:48 +0300

    * LongestCommonSubsequence

--- src/main/puzzler/leetcode/dynamic/programming/LongestCommonSubsequence.java
+++ src/main/puzzler/leetcode/dynamic/programming/LongestCommonSubsequence.java
@@ -20,7 +20,8 @@ public class LongestCommonSubsequence {
                 {"abc", "ac", 2},
                 {"abcr", "zbck", 2},
                 {"abba", "bb", 2},
-                {"aba", "bb", 1}
+                {"aba", "bb", 1},
+                {"acdb", "abcd", 3}
         };
     }
 
@@ -29,24 +30,31 @@ public class LongestCommonSubsequence {
         assertEquals(getLongestCommonSubsequence(a, b), count);
     }
 
+    /**
+     * Dynamic solution
+     * lcs[a+1][b+1] - is max num of LCS
+     *
+     * if(a[i]=b[j]) lcs[a+1][b+1] = lcs[a+1][b+1] = lcs[a][b] + 1
+     * else lcs[a+1][b+1] = max(lcs[a][b+1], lcs[a+1][b])
+     */
     public int getLongestCommonSubsequence(String a, String b) {
-        int count = 0;
+        int[][] lcs = new int[a.length() + 1][b.length() + 1];
 
         char[] charsA = a.toCharArray();
         char[] charsB = b.toCharArray();
 
-        int nextAJ = 0;
-
-        for (int i = 0; i < charsB.length; i++) {
-            for (int j = nextAJ; j < charsA.length; j++) {
-                if (charsB[i] == charsA[j]) {
-                    count++;
-                    nextAJ = j + 1;
-                    break;
+        for (int i = 0; i <= charsA.length; i++) {
+            for (int j = 0; j <= charsB.length; j++) {
+                if (i == 0 || j == 0) {
+                    lcs[i][j] = 0;
+                } else if (charsA[i - 1] == charsB[j - 1]) {
+                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                } else {
+                    lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
                 }
             }
         }
 
-        return count;
+        return lcs[a.length()][b.length()];
     }
 }
